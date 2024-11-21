07e478801154d032ff0dfb8e199ea54b824e5539
DaSmSchool <145377174+DaSmSchool@users.noreply.github.com>
2024-11-21 15:27:16 -0500

    finish part B, add progress to challenge 5 in part C

--- index.js
+++ index.js
@@ -5,7 +5,29 @@ ulEl = document.getElementById("ul-el")
 listItems = ""
 myLeads = []
 
+function init() {
+    if (localStorage.listItems == undefined) {
+        initLocalStorage()
+    } else {
+        loadLocalStorage()
+    }
+}
+
+function initLocalStorage() {
+    localStorage.setItem("listItems", '')
+    loadLocalStorage()
+}
+
+function loadLocalStorage() {
+    listItems = loadLocalStorage.listItems
+}
+
+function updateLocalStorage() {
+    localStorage.listItems = listItems
+}
+
 function updateList() {
+    updateLocalStorage()
     ulEl.innerHTML = listItems
 }
 
@@ -13,10 +35,13 @@ function onSave() {
     myLeads.push(linkInput.value)
 
     console.log("Button clicked!")
-    listItems += `<li>${linkInput.value}</li>`
+    listItems += `<li><a href="${linkInput.value}" target='_blank'>${linkInput.value}</a></li>`
     updateList()
 }
 
 saveBtn.addEventListener("click", function() {
     onSave()
+    linkInput.value = ""
 })
+
+init()
